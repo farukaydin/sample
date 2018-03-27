5a24f4f0977a4868aaea592acac56debb6a167f4
Faruk AYDIN <omerfaruk26@gmail.com>
2018-03-27 14:24:11 +0200

    Complete first version of draft api resource

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,49 @@
+class Api::V1::UsersController < Api::V1::BaseController
+  before_action :assign_user, only: [:show, :update, :destroy]
+
+  def index
+    @users = User.all
+
+    render json: @users.to_json
+  end
+
+  def show
+    render json: @user.to_json
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      return head :created
+    end
+
+    render json: { errors: @user.errors }.to_json
+  end
+
+  def update
+    if @user.update(user_params)
+      return render json: @user.to_json
+    end
+
+    render json: { errors: @user.errors }.to_json
+  end
+
+  def destroy
+    if @user.destroy
+      return head :no_content
+    end
+
+    render json: { errors: @user.errors }.to_json
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :age)
+  end
+
+  def assign_user
+    @user = User.find(params[:id])
+  end
+end
--- app/controllers/users_controller.rb
+++
@@ -1,5 +0,0 @@
-class UsersController < ApplicationController
-  def index
-    render json: { "Hello" => "world!" }.to_json
-  end
-end
