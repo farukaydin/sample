b070b59d7b3557397488c42a33b0a4182d1f2e25
Faruk AYDIN <omerfaruk26@gmail.com>
2018-03-28 14:27:52 +0200

    Add project model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  has_many :users
+
   validates :name, :age, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180327092515) do
+ActiveRecord::Schema.define(version: 20180328122651) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
