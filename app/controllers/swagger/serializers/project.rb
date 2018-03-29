28623e4891dbd69c89f5b8d273f4f6b7003c9dd8
Faruk AYDIN <omerfaruk26@gmail.com>
2018-03-29 15:58:43 +0200

    Add serializers to swagger doc tool

---
+++ app/controllers/swagger/serializers/project.rb
@@ -0,0 +1,16 @@
+module Swagger::Serializers
+  class Project
+    include Swagger::Blocks
+
+    swagger_schema :Project do
+      key :required, [:id, :name]
+      property :id do
+        key :type, :integer
+        key :format, :int64
+      end
+      property :name do
+        key :type, :string
+      end
+    end
+  end
+end
---
+++ app/controllers/swagger/serializers/user.rb
@@ -0,0 +1,26 @@
+module Swagger::Serializers
+  class User
+    include Swagger::Blocks
+
+    swagger_schema :User do
+      key :required, [:id, :name, :age]
+      property :id do
+        key :type, :integer
+        key :format, :int64
+      end
+      property :name do
+        key :type, :string
+      end
+      property :age do
+        key :type, :integer
+        key :format, :int32
+      end
+      property :projects do
+        key :type, :array
+        items do
+          key :'$ref', :Project
+        end
+      end
+    end
+  end
+end
